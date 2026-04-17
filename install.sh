1d69e795f8e38da69c439177b0266b5c7eb56938
Kristina Schneider <me@kristinaschneider.com>
2026-04-17 17:10:13 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-cat <<'EOF' > ~/dotfiles/install.sh
 #!/bin/bash
 cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/bashrc ~/.bashrc
 cp ~/dotfiles/gitconfig ~/.gitconfig
 cp ~/dotfiles/gitignore_global ~/.gitignore_global
-EOF
-
-chmod +x ~/dotfiles/install.sh
